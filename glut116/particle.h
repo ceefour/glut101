c266e6bb88b945f28b19b421c6e456c983ac4f58
Hendy Irawan <ceefour666@gmail.com>
2015-03-19 17:49:29 +0700

    program 16 - particle system
    Signed-off-by: Hendy Irawan <ceefour666@gmail.com>

---
+++ glut116/particle.h
@@ -0,0 +1,15 @@
+#ifndef PARTICLE_H_
+#include <cstdbool>
+typedef struct
+{
+	float lifetime; // total lifetime of the particle
+	float decay; // decay speed of the particle
+	float r, g, b; // color values of the particle
+	float xpos, ypos, zpos; // position of the particle
+	float xspeed, yspeed, zspeed; // speed of the particle
+	boolean active; // is particle active or not?
+} PARTICLE;
+void CreateParticle(int i);
+void InitParticle();
+void EvolveParticle();
+#endif
